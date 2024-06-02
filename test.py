1e80b138b44b633da5379eadda2c9857c12b39fa
Danil Galperin <Misterrendal@gmail.com>
2024-06-02 18:01:28 +0300

    fixed init_worker in test.py

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ from src.sgd_allocation import SGDAllocation
 class AllocationProcess:
     def __init__(self):
         self._model = RandomForestAllocation()
-        self._sgd = SGDAllocation()
+        self._sgd = SGDAllocation(num_cpu=2)
         print("PID:", os.getpid())
 
     def process(self, assets_and_pools):
@@ -38,11 +38,10 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--processes', type=int, help='number of processes to use', default=10)
+    parser.add_argument('--processes', type=int, help='number of processes to use', default=6)
     args = parser.parse_args()
 
-    pool = multiprocessing.Pool(processes=args.processes, initializer=init_worker(),
-                                initargs=(f"Worker-{os.getpid()}",))
+    pool = multiprocessing.Pool(processes=args.processes, initializer=init_worker, initargs=())
 
     threads = []
 
